a82eddea56ab82af125106a3c3390111b9ed9d7e
Adi016prasad <130592410+Adi016prasad@users.noreply.github.com>
2026-05-28 13:26:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -36,5 +37,6 @@ def fetch_policy():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
